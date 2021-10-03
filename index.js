14c0737e83f87f0f45d3786f497c66fcd230cae6
ELK4N4 <elkana546@gmail.com>
2021-10-03 20:48:41 +0300

    fixed bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 const registerScheme = yup.object({
-    username: yup.string().min(3, "Please enter a username longer than 3").required("Username must be required!");
+    username: yup.string().min(3, "Please enter a username longer than 3").required("Username must be required!"),
     email: yup.string().email(3, "Please enter a vali").required("Username must be required!"),
     password: yup.string().required(),
     confirmPassword: yup.string().when("password", {
