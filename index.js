223fbcacb60bf8e5b945d833f85daa7309bf360f
ELK4N4 <elkana546@gmail.com>
2021-10-03 20:28:17 +0300

    added regiter scheme

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function isTrue(bool) {
-    return bool === true
-}
+import * as yup from "yup";
 
-module.exports = isTrue
+const registerScheme = yup.object({
+    username: yup.string().min(3, "Please enter a username longer than 3").required("Username must be required!");
+    email: yup.string().email(3, "Please enter a vali").required("Username must be required!"),
+    password: yup.string().required(),
+    confirmPassword: yup.string().when("password", {
+        is: val => (val && val.length > 0 ? true : false),
+        then: yup.string().oneOf([yup.ref("password")], "Password does not match")
+    }),
+})
+
+module.exports = registerScheme
