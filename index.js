b06cd2be7c4ee3f607b67fd86f20775c99f59727
ELK4N4 <elkana546@gmail.com>
2021-10-03 22:00:21 +0300

    fixed bug

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import * as yup from "yup";
 
-const registerScheme = yup.object({
+export const registerScheme = yup.object({
+    email: yup.string().email("Please enter a valid email").required("Email must be required!"),
     username: yup.string().min(3, "Please enter a username longer than 3").required("Username must be required!"),
-    email: yup.string().email(3, "Please enter a vali").required("Username must be required!"),
     password: yup.string().required(),
     confirmPassword: yup.string().when("password", {
         is: val => (val && val.length > 0 ? true : false),
         then: yup.string().oneOf([yup.ref("password")], "Password does not match")
     }),
-})
-
-module.exports = registerScheme
+});
