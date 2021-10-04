c6cd72af517307a6f9f246d1bca0623ff8f725a0
ELK4N4 <elkana546@gmail.com>
2021-10-04 13:32:06 +0300

    added login scheme

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as yup from "yup";
 
+export const loginScheme = yup.object({
+    email: yup.string().email("Please enter a valid email").required("Email must be required!"),
+    password: yup.string().required(),
+});
+
 export const registerScheme = yup.object({
     email: yup.string().email("Please enter a valid email").required("Email must be required!"),
     username: yup.string().min(3, "Please enter a username longer than 3").required("Username must be required!"),
