df9175b1bab672386aadc698f580588537eeef99
ELK4N4 <elkana546@gmail.com>
2021-10-05 18:30:13 +0300

    added anime shceme

--- validations/index.js
+++ validations/index.js
@@ -18,7 +18,7 @@ export const registerScheme = yup.object({
     confirmPassword: yup.string().when("password", {
         is: val => (val && val.length > 0 ? true : false),
         then: yup.string().oneOf([yup.ref("password")], "Password does not match"),
-    }),
+    }).required("Confirm password must be required!"),
 });
 
 export const userUpdateScheme = yup.object({
@@ -36,4 +36,16 @@ export const fansubScheme = yup.object({
     name: yup.string().min(2, "Please enter a fansub name longer than 2").required("Name must be required!"),
     avatar: yup.string().url("Please enter a valid url image"),
     banner: yup.string().url("Please enter a valid url image"),
+});
+
+export const animeScheme = yup.object({
+    name: yup.object({
+        hebrew: yup.string().min(2, "Please enter a Hebrew name longer than 3").required("Hebrew name must be required!"),
+        english: yup.string().min(2, "Please enter a English name longer than 3").required("English name must be required!"),
+        japanese: yup.string().min(2, "Please enter a Japanese name longer than 3").required("Japanese name must be required!"),
+    }),
+    genres: yup.array().min(1).required("Genres number must be required!"),
+    episodesNumber: yup.number().min(1).required("Episodes number must be required!"),
+    summary: yup.string().min(10, "Please enter a summary longer than 10").required("Summary must be required!"),
+    image: yup.string().url("Please enter a valid url image"),
 });
