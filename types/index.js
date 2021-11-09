c210494ea1c8e47d97bdd2867a13d8a19d7c3c79
ELK4N4 <elkana546@gmail.com>
2021-11-09 15:44:52 +0200

    changed file extentions to js

---
+++ types/index.js
@@ -0,0 +1,14 @@
+
+export const FANSUB = { text: 'פאנסאב', tooltip: 'שינוי שם ותמונה של הפאנסאב' };
+export const PROJECTS = { text: 'פרוייקטים', tooltip: 'הוספת, מחיקת ועריכת פרוייקטים' };
+export const MEMBERS = { text: 'צוות', tooltip: 'הוספת, מחיקת ועריכת חברי צוות' };
+
+const fansub = { FANSUB, PROJECTS, MEMBERS };
+
+export const ADMINS = { text: 'אדמינים', tooltip: 'הוספת אדמינים ועריכת הרשאות' };
+export const BANS = { text: 'באנים', tooltip: 'הוספת, מחיקת ועריכת באנים' };
+export const ANIMES = { text: 'אנימות', tooltip: 'הוספת, מחיקת ועריכת אנימות במאגר' };
+
+const admin = { ADMINS, BANS, ANIMES };
+
+export const permissionsTypes = {admin, fansub};
