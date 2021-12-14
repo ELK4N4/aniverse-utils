367269373db5f644ab666ce172b4e25fb068d602
ELK4N4 <elkana546@gmail.com>
2021-12-14 07:08:25 +0200

    added FANSUBS perimission in types

--- types/index.js
+++ types/index.js
@@ -7,8 +7,9 @@ const fansub = { FANSUB, PROJECTS, MEMBERS };
 
 export const ADMINS = { text: 'אדמינים', tooltip: 'הוספת אדמינים ועריכת הרשאות' };
 export const BANS = { text: 'באנים', tooltip: 'הוספת, מחיקת ועריכת באנים' };
+export const FANSUBS = { text: 'פאנסאבים', tooltip: 'אישור ודחייה של בקשות לפתיחת פאנסאבים' };
 export const ANIMES = { text: 'אנימות', tooltip: 'הוספת, מחיקת ועריכת אנימות במאגר' };
 
-const admin = { ADMINS, BANS, ANIMES };
+const admin = { ADMINS, BANS, FANSUBS, ANIMES };
 
 export const permissionsTypes = {admin, fansub};
